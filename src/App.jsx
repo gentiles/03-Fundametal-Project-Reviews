0f6044c3a6b078493d53a75f203869f2f5f4be5a
erwin <erwinmarin05@gmail.com>
2024-10-19 15:44:33 +0800

    imported data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from 'react'
+import reviews from './data'
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
-};
-export default App;
+  const [person, setPerson] = useState(reviews)
+
+  const listPerson = person.map((list) => {
+    const { id, name, job, image } = list
+    return (
+      <div>
+        <h3>{name}</h3>
+      </div>
+    )
+  })
+
+  return (
+    <section>
+      <h2>Reviews Starter</h2>
+      {listPerson}
+    </section>
+  )
+}
+export default App
