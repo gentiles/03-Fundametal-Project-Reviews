d7a8f6ae93cab4c74584fb9221cfffd007ad78c4
erwin <erwinmarin05@gmail.com>
2024-10-19 16:16:57 +0800

    adding name

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
 import { useState } from 'react'
-import reviews from './data'
+import person from './data'
 
 const App = () => {
-  const [person, setPerson] = useState(reviews)
+  const [index, setIndex] = useState(0)
+  const { name, job, image, text } = person[index]
 
-  const listPerson = person.map((list) => {
-    const { id, name, job, image } = list
-    return (
-      <div>
-        <h3>{name}</h3>
-      </div>
-    )
-  })
+  // const listPerson = person.map((list) => {
+  //   const { id, name, job, image } = list
+  //   return (
+  //     <div>
+  //       <h3>{name}</h3>
+  //     </div>
+  //   )
+  // })
 
+  function handleAdd() {
+    if (index <= 4) {
+      setIndex((prev) => prev + 1)
+      return
+    }
+    return
+  }
+  function handleSub() {
+    if (index > 0) {
+      return setIndex((prev) => prev - 1)
+    }
+  }
+
+  // const listItems = person[index].name
+  console.log(name)
   return (
     <section>
       <h2>Reviews Starter</h2>
-      {listPerson}
+
+      <button type="button" style={{ padding: '0.5rem' }} onClick={handleAdd}>
+        +
+      </button>
+      <button type="button" style={{ padding: '0.5rem' }} onClick={handleSub}>
+        -
+      </button>
     </section>
   )
 }
